8d45dd2ef02a034f3edfd14b5227e1ed656f81ad
minFe17 <vvcx1234@naver.com>
2025-06-05 14:30:19 +0900

    플레이어 이동 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D _rigidbody;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] float _speed;
+
+    float _movePos;
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        _rigidbody.linearVelocityX = _movePos * _speed;
+    }
+
+    void Turn()
+    {
+        if (_movePos < 0)
+            _spriteRenderer.flipX = true;
+        else
+            _spriteRenderer.flipX = false;
+    }
+
+
+    // InputSystem
+    void OnMove(InputValue value)
+    {
+        _movePos = value.Get<Vector2>().x;
+        if(_movePos != 0)
+            Turn();
+    }
+
+    
+}
