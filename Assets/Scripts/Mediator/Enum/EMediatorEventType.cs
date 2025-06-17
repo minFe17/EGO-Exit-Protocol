06d91a2b98407861cbb98da4fe786179cded0a99
minFe17 <vvcx1234@naver.com>
2025-06-17 21:32:51 +0900

    깃으로 인한 버그

--- Assets/Scripts/Mediator/Enum/EMediatorEventType.cs
+++ Assets/Scripts/Mediator/Enum/EMediatorEventType.cs
@@ -6,20 +6,10 @@ public enum EMediatorEventType
     TimeTick,
     ChangeLoopCount,
     AddMemory,
-<<<<<<< HEAD
-<<<<<<< HEAD
     NeedCapture,       
     CompleteCapture,
-    Max,
-=======
-=======
->>>>>>> origin/main
     PlayerEnterAssistantRoom,
     PlayerExitAssistantRoom,
     RopeReleased,
-    Max
-<<<<<<< HEAD
->>>>>>> 0b7ef7a1e6d38efe2e99d4cf9ee458166c630081
-=======
->>>>>>> origin/main
+    Max,
 }
