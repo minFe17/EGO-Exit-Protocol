bc7bf24d66450379450fa8d6cf4dd610d0f8c8c5
minFe17 <119782382+minFe17@users.noreply.github.com>
2025-07-08 17:19:25 +0900

    코드 정리

--- Assets/Scripts/Ending/EndingManager.cs
+++ Assets/Scripts/Ending/EndingManager.cs
@@ -17,10 +17,6 @@ public class EndingManager : MonoBehaviour, IMediatorEvent
     void IMediatorEvent.HandleEvent(object data)
     {
         _endingType = (EEndingType)data;
-        // 페이드 연출?
-        // 엔딩처리
-
-        // 중재자 이벤트 다 삭제
         GenericSingleton<MediatorManager>.Instance.ClearMediatorEvent();
         SceneManager.LoadScene("EndingScene");
     }
