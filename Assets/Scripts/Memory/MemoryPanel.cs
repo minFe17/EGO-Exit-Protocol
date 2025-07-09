3a155053d944865cf57fd57a3cfa7c5e2eed76a6
minFe17 <vvcx1234@naver.com>
2025-07-09 18:55:43 +0900

    증거 추가

--- Assets/Scripts/Memory/MemoryPanel.cs
+++ Assets/Scripts/Memory/MemoryPanel.cs
@@ -6,7 +6,6 @@ using Utils;
 
 public class MemoryPanel : MonoBehaviour, IMemento
 {
-    [SerializeField] RectTransform _rectTransform;
     [SerializeField] Image _image;
     [SerializeField] Text _description;
 
@@ -15,6 +14,7 @@ public class MemoryPanel : MonoBehaviour, IMemento
     MemoryManager _memoryManager;
     MemoryData _memoryData;
     BoardUI _board;
+    RectTransform _rectTransform;
 
     Vector2 _lastMousePosition;
     Vector2 _halfSize;
@@ -26,6 +26,7 @@ public class MemoryPanel : MonoBehaviour, IMemento
     {
         _board = parent;
         _rect = _board.GetComponent<RectTransform>().rect;
+        _rectTransform = GetComponent<RectTransform>();
         _memoryPanelData = memoryPanelData;
         _memoryManager = GenericSingleton<MemoryManager>.Instance;
         _memoryData = _memoryManager.MemoryRepository.GetMemoryData(_memoryPanelData.MemoryType);
